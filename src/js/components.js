2319da404253ea86ca3e0f45519cfc933ab0c1a2
JesusAcostaArevalo <jesusacosta@wposs.com>
2022-02-10 16:38:42 -0500

    Se agregan funcionalidades del filtro de tareas

--- src/js/components.js
+++ src/js/components.js
@@ -16,6 +16,7 @@ export const ACCIONES = {
 
 const infoTarea = document.querySelector(".new-todo");
 const ulTareas = document.querySelector(".todo-list");
+const filtro = document.querySelector(".filters");
 
 // TODO: Evento agregar nueva tarea
 infoTarea.addEventListener("keyup", ($event) => {
@@ -23,7 +24,7 @@ infoTarea.addEventListener("keyup", ($event) => {
   const valorTarea = $event.target.value;
   const tarea = new Tarea(valorTarea);
 
-  infoTarea.value = '';
+  infoTarea.value = "";
 
   listaTareas.addTarea(tarea);
   mostraTareas(); // TODO: realizar instancia clase tarea y pasas el valor
@@ -32,10 +33,46 @@ infoTarea.addEventListener("keyup", ($event) => {
 // TODO: Evento para completar y eliminar tarea
 ulTareas.addEventListener("click", ($event) => {
   const tipoElemento = $event.target.localName;
-
   const tarea = $event.target.parentElement.parentElement;
   const idTarea = tarea.getAttribute("data-id");
-  tarea.classList.toggle("completed");
 
-  if (tipoElemento !== "label") ACCIONES[tipoElemento](idTarea);
+  if (tipoElemento === "label") return;
+
+  ACCIONES[tipoElemento](idTarea);
+  tarea.classList.toggle("completed");
+});
+
+const FUNCIONES = {
+  pendientes: (arrayLis) => {
+    for (const iterator of arrayLis) {
+      const claseIterator = iterator.getAttribute("class");
+      if (claseIterator === "completed") {
+        iterator.classList.add("hidden");
+      }
+    }
+  },
+
+  todos: (arrayLis) => {
+    for (const iterator of arrayLis) {
+      if (iterator.classList.contains("hidden")) {
+        iterator.classList.remove("hidden");
+      }
+    }
+  },
+  completados: (arrayLis) => {
+    for (const iterator of arrayLis) {
+      const claseIterator = iterator.getAttribute("class");
+      if (claseIterator !== "completed") {
+        iterator.classList.add("hidden");
+      }
+    }
+  }
+};
+
+//TODO: Evento para listar la lista de tareas pendientes:
+filtro.addEventListener("click", ($event) => {
+  const tipoLi = $event.target.textContent.toLowerCase();
+  const arrayLis = ulTareas.children;
+
+  FUNCIONES[tipoLi](arrayLis);
 });
